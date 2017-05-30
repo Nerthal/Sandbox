042a8c9967e45d9038e98d5cf40e91c2f6c6cb11
Tomasz Górecki <tmsz.gorecki@gmail.com>
2017-05-31 00:45:31 +0200

    Added spring properties file.

--- IdeaProjects/selenium/src/test/java/Demo/SimpleClassTest.java
+++ IdeaProjects/selenium/src/test/java/Demo/SimpleClassTest.java
@@ -2,6 +2,8 @@ package demo;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
 import pageObject.GoogleSearchPage;
 import org.junit.After;
 import org.junit.Assert;
@@ -11,13 +13,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import pageObject.ResultCountEvaluator;
 
-import java.util.Arrays;
-import java.util.Collection;
 
-@RunWith(value = Parameterized.class)
+@TestPropertySource("/test.properties")
+//@RunWith(value = Parameterized.class)
 public class SimpleClassTest {
     private RemoteWebDriver driver;
     private GoogleSearchPage googleSearchPage;
+
+    @Value("${range}")
+    private int range;
 
     @Before
     public void setUp() {
@@ -25,30 +29,30 @@ public class SimpleClassTest {
         googleSearchPage = new GoogleSearchPage(driver);
     }
 
-    @Parameterized.Parameter(value = 0)
-    public String cityName;
-
-    @Parameterized.Parameter(value = 1)
-    public int resultCount;
-
-    @Parameterized.Parameter(value = 2)
-    public int resultRange;
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {"szczebrzeszyn", 2040000, 10000},
-                {"Piekary Śląskie", 5200000, 60000},
-                {"Tychy", 17000000, 100000},
-                {"Bytom", 20000000, 500000},
-                {"Katowice", 62000000, 200000}
-        });
-    }
+//    @Parameterized.Parameter(value = 0)
+//    public String cityName;
+//
+//    @Parameterized.Parameter(value = 1)
+//    public int resultCount;
+//
+//    @Parameterized.Parameter(value = 2)
+//    public int resultRange;
+//
+//    @Parameterized.Parameters
+//    public static Collection<Object[]> data() {
+//        return Arrays.asList(new Object[][]{
+//                {"szczebrzeszyn", 2040000, 10000},
+//                {"Piekary Śląskie", 5200000, 60000},
+//                {"Tychy", 17000000, 100000},
+//                {"Bytom", 20000000, 500000},
+//                {"Katowice", 62000000, 200000}
+//        });
+//    }
 
     @Test
     public void numberOfResultsShouldMatch() {
-        ResultCountEvaluator evaluator = new ResultCountEvaluator(resultCount, resultRange);
-        Assert.assertTrue(evaluator.areEqual(googleSearchPage.search(cityName).getResultsCount()));
+        ResultCountEvaluator evaluator = new ResultCountEvaluator(2040000, range);
+        Assert.assertTrue(evaluator.areEqual(googleSearchPage.search("szczebrzeszyn").getResultsCount()));
     }
 
     @After

f17970008692e9182ad495d2971d113c4c7b0c70
Tomasz Górecki <tmsz.gorecki@gmail.com>
2017-05-30 22:40:32 +0200

    Parametrized tests

--- IdeaProjects/selenium/src/test/java/Demo/SimpleClassTest.java
+++ IdeaProjects/selenium/src/test/java/Demo/SimpleClassTest.java
@@ -1,5 +1,7 @@
 package demo;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import pageObject.GoogleSearchPage;
 import org.junit.After;
 import org.junit.Assert;
@@ -9,24 +11,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import pageObject.ResultCountEvaluator;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
 public class SimpleClassTest {
-	private RemoteWebDriver driver;
-	private GoogleSearchPage googleSearchPage;
+    private RemoteWebDriver driver;
+    private GoogleSearchPage googleSearchPage;
 
-	@Before
-	public void setUp() {
-		driver = new ChromeDriver();
-		googleSearchPage = new GoogleSearchPage(driver);
-	}
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        googleSearchPage = new GoogleSearchPage(driver);
+    }
 
-	@Test
-	public void numberOfResultsShouldMatch() {
-		ResultCountEvaluator evaluator = new ResultCountEvaluator(2040000, 10000);
-		Assert.assertTrue(evaluator.areEqual(googleSearchPage.search("szczebrzeszyn").getResultsCount()));
-	}
+    @Parameterized.Parameter(value = 0)
+    public String cityName;
 
-	@After
-	public void tearDown() {
-		driver.quit();
-	}
+    @Parameterized.Parameter(value = 1)
+    public int resultCount;
+
+    @Parameterized.Parameter(value = 2)
+    public int resultRange;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"szczebrzeszyn", 2040000, 10000},
+                {"Piekary Śląskie", 5200000, 60000},
+                {"Tychy", 17000000, 100000},
+                {"Bytom", 20000000, 500000},
+                {"Katowice", 62000000, 200000}
+        });
+    }
+
+    @Test
+    public void numberOfResultsShouldMatch() {
+        ResultCountEvaluator evaluator = new ResultCountEvaluator(resultCount, resultRange);
+        Assert.assertTrue(evaluator.areEqual(googleSearchPage.search(cityName).getResultsCount()));
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }

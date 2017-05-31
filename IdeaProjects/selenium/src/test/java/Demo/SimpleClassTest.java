fbe051612bd0eae5a56c843f0dd307b37b09b194
Piotr Klubinski <piotr.klubinski@jamfsoftware.com>
2017-05-31 14:04:52 +0200

    parametrized spring test

--- IdeaProjects/selenium/src/test/java/Demo/SimpleClassTest.java
+++ IdeaProjects/selenium/src/test/java/Demo/SimpleClassTest.java
@@ -1,9 +1,11 @@
 package demo;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
 import pageObject.GoogleSearchPage;
 import org.junit.After;
 import org.junit.Assert;
@@ -11,48 +13,52 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContextManager;
+
 import pageObject.ResultCountEvaluator;
 
-
-@TestPropertySource("/test.properties")
-//@RunWith(value = Parameterized.class)
+@ContextConfiguration(classes = TestConfig.class)
+@RunWith(value = Parameterized.class)
 public class SimpleClassTest {
     private RemoteWebDriver driver;
     private GoogleSearchPage googleSearchPage;
 
-    @Value("${range}")
-    private int range;
+    @Autowired
+    private ResultCountEvaluator resultCountEvaluator;
 
-    @Before
-    public void setUp() {
-        driver = new ChromeDriver();
+	private TestContextManager testContextManager;
+
+	@Before
+    public void setUp() throws Exception {
+
+		this.testContextManager = new TestContextManager(getClass());
+		this.testContextManager.prepareTestInstance(this);
+
+		driver = new ChromeDriver();
         googleSearchPage = new GoogleSearchPage(driver);
     }
 
-//    @Parameterized.Parameter(value = 0)
-//    public String cityName;
-//
-//    @Parameterized.Parameter(value = 1)
-//    public int resultCount;
-//
-//    @Parameterized.Parameter(value = 2)
-//    public int resultRange;
-//
-//    @Parameterized.Parameters
-//    public static Collection<Object[]> data() {
-//        return Arrays.asList(new Object[][]{
-//                {"szczebrzeszyn", 2040000, 10000},
-//                {"Piekary Śląskie", 5200000, 60000},
-//                {"Tychy", 17000000, 100000},
-//                {"Bytom", 20000000, 500000},
-//                {"Katowice", 62000000, 200000}
-//        });
-//    }
+    @Parameterized.Parameter(value = 0)
+    public String cityName;
+
+    @Parameterized.Parameter(value = 1)
+    public int resultCount;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"szczebrzeszyn", 2040000},
+                {"Piekary Śląskie", 5200000},
+                {"Tychy", 17000000},
+                {"Bytom", 20000000},
+                {"Katowice", 62000000}
+        });
+    }
 
     @Test
     public void numberOfResultsShouldMatch() {
-        ResultCountEvaluator evaluator = new ResultCountEvaluator(2040000, range);
-        Assert.assertTrue(evaluator.areEqual(googleSearchPage.search("szczebrzeszyn").getResultsCount()));
+        Assert.assertTrue(resultCountEvaluator.areEqual(resultCount, googleSearchPage.search("szczebrzeszyn").getResultsCount()));
     }
 
     @After
